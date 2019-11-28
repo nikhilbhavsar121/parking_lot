209ea5d4dbdff38bac6a3be4df6960750835df32
Nikhil Bhavsar <nikhilbhavsar121@gmail.com>
2019-11-28 22:46:54 +0530

    Add commands in map

--- src/main/java/com/gojek/parkinglot/CommandProcess.java
+++ src/main/java/com/gojek/parkinglot/CommandProcess.java
@@ -20,5 +20,9 @@ public class CommandProcess
         commandMap.put("create_parking_lot",ParkingLot.class.getMethod("createParkingLot", String.class));
         commandMap.put("park",ParkingLot.class.getMethod("park", String.class,String.class));
         commandMap.put("leave",ParkingLot.class.getMethod("leave", String.class));
+        commandMap.put("status",ParkingLot.class.getMethod("status"));
+        commandMap.put("registration_numbers_for_cars_with_colour", ParkingLot.class.getMethod("getRegistrationNumbersFromColor", String.class));
+        commandMap.put("slot_numbers_for_cars_with_colour",ParkingLot.class.getMethod("getSlotNumbersWithGivenColor",String.class));
+        commandMap.put("slot_number_for_registration_number",ParkingLot.class.getMethod("getSlotNumberForRegNo",String.class));
     }
 }
